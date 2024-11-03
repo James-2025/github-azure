9bc76f1f8c2364bb942a2b5211380f831c9ed175
James-2025 <4al20cs052@gmail.com>
2024-11-03 16:38:33 +0530

    c02

--- main.tf
+++ main.tf
@@ -1,94 +1,77 @@
-terraform {
-  backend "s3" {
-    bucket         = "champions-bucket"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
+provider "azurerm" {
+  features {}
+}
+
+variable "vm_name" {
+  default = "my-azure-vm"
+}
+
+variable "resource_group_name" {
+  default = "my-terraform-rg"
+}
+
+variable "location" {
+  default = "eastus"
+}
+
+variable "admin_username" {
+  default = "azureuser"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.resource_group_name}-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.vm_name}-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
   }
 }
 
-provider "aws" {
-  region = var.aws_region
-}
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = var.vm_name
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  size                  = "Standard_B1s"
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"]  # Canonical's AWS account ID for Ubuntu AMIs
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-}
-
-resource "aws_security_group" "ssh_access" {
-  name        = "allow_ssh"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Consider restricting this to GitHub Actions IP ranges for better security
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-
-resource "aws_instance" "example" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-
-  # SSH connection configuration for provisioners
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"  # Default user for Ubuntu AMIs
-    private_key = file(var.private_key_path)  # Path to the SSH private key
-    host        = self.public_ip
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
 
-
-  provisioner "local-exec" {
-  command = "sleep 60"  # Waits 60 seconds
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
   }
 
-  provisioner "file" {
-    #source      = "index.html"
-    #destination = "/var/www/html/index.html"
-    source      = "index.html"
-    destination = "/tmp/index.html"  # Use a temporary directory
-  
-  }
+  admin_username = var.admin_username
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt update -y",
-      "sudo apt install -y apache2",
-      #"sudo mv /var/www/html/index.html /var/www/html/",
-      "sudo mv /tmp/index.html /var/www/html/index.html",  # Move it with sudo
-      "sudo chown www-data:www-data /var/www/html/index.html",  # Set ownership
-      "sudo systemctl start apache2",
-      "sudo systemctl enable apache2"
-    ]
-  }
-
-  tags = {
-    Name = var.name
-  }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt update -y
-              sudo apt install -y apache2
-              sudo systemctl start apache2
-              sudo systemctl enable apache2
-              EOF
-
-  lifecycle {
-    create_before_destroy = true
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = "testkey"
   }
 }
