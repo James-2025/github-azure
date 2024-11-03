873cbf12a51cc5ad6712254a594adc12b3d36694
James-2025 <4al20cs052@gmail.com>
2024-11-04 00:15:09 +0530

    c38

--- main.tf
+++ main.tf
@@ -99,8 +99,6 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id  # Associate with public IP
   }
-    network_security_group_id = azurerm_network_security_group.nsg.id
-
 }
 
 resource "azurerm_subnet_network_security_group_association" "subnet_nsg_assoc" {
