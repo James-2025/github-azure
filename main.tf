de28c733ca791f2d506b07f60669f0a131845efa
James-2025 <4al20cs052@gmail.com>
2024-11-03 23:42:49 +0530

    c34

--- main.tf
+++ main.tf
@@ -16,6 +16,13 @@ variable "vm_name" {
   default = "my-azure-vm01"
 }
 
+variable "ssh_public_key" {
+  description = "The SSH public key used for authentication."
+  type        = string
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDFSz/3f6ccuQsSaMjasW9P+NzYjVaokcqDdZ7nh6n3VlQFCLRBMTIjj82ohQm1bbdlTd38mmhOm8CThar4rfXnnF0UxNhTK41LDp678wepxTK0MkbeaQpgOMdSAOolGvPtKxppbrk7KK65+zeH2J/1pH+4bkwn3kaagnGtZir0uOJRBFAJAUNACplWEbTtYdNQ+TNn7DNeXu9ew+949953bWCVubRUVia+sdF0HPFqMzeGPNZL3PxZMs+uCzNl952pj+wSqOyMMK15+UoPMuLX+KT3iRjigeugaAxSdnhkBVH734DEjk3qb6wpGtFv58zfwbwrVSowMbxTMJyzpc4I9Loq4yuapd6utLGshUSALdUUbCdCFcp+IoDNqFvuSSKRpmPrYJnicrPzKVpLqtwfuRk12p07c5jSIgX7Y0DC5GQi4MNqyfBJVCki8u++vlmLrxAxVcxmNJGQLL2bOuvfYbI0Xd+FzjRadilBoWUUHzloQC/4DnGVGBc6WXokezE= generated-by-azure"
+}
+
+
 variable "resource_group_name" {
   default = "my-terraform-rg"
 }
@@ -101,7 +108,7 @@ resource "azurerm_virtual_machine" "vm" {
 
     ssh_keys {
       path     = "/home/${var.admin_username}/.ssh/authorized_keys"
-      key_data = file(var.private_key_path)  # Use the variable for the SSH key
+      key_data = var.ssh_public_key
     }
   }
 
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,5 +1,5 @@
 #key_name       = "2024"
 #instance_type = "t2.micro"
 #aws_region = "us-east-1"
-#private_key_path = "~/.ssh/2024.pem"
+private_key_path = "~/.ssh/2024.pem"
 #name="prod-instance"
