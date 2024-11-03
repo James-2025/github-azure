cf3291199713400de9e649e89a889b1296b6837a
James-2025 <4al20cs052@gmail.com>
2024-11-03 19:11:04 +0530

    c13

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ variable "resource_group_name" {
 }
 
 variable "location" {
-  default = "westindia"
+  default = "westus2"
 }
 
 variable "admin_username" {
@@ -59,7 +59,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
-  size                  = "Standard_B1s"
+  size                  = "Standard_D2s_v3"
 
   os_disk {
     caching              = "ReadWrite"
