9b4d0a086ba971e8aef0f6070f2719dfb0fbba4d
James-2025 <4al20cs052@gmail.com>
2024-11-03 23:59:18 +0530

    c35

--- main.tf
+++ main.tf
@@ -35,6 +35,27 @@ variable "admin_username" {
   default = "azureuser"
 }
 
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.vm_name}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_security_rule" "ssh" {
+  name                        = "allow-ssh"
+  priority                    = 1000
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+
+
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
@@ -75,6 +96,8 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id  # Associate with public IP
   }
+    network_security_group_id = azurerm_network_security_group.nsg.id
+
 }
 
 resource "azurerm_virtual_machine" "vm" {
