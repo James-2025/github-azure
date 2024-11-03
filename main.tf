824854b2d10d57074200dd93eb788e8229cde57e
James-2025 <4al20cs052@gmail.com>
2024-11-04 00:08:12 +0530

    c37

--- main.tf
+++ main.tf
@@ -75,7 +75,6 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
-  network_security_group_id = azurerm_network_security_group.nsg.id
 
 
   depends_on = [azurerm_virtual_network.vnet]
@@ -102,6 +101,11 @@ resource "azurerm_network_interface" "nic" {
   }
     network_security_group_id = azurerm_network_security_group.nsg.id
 
+}
+
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg_assoc" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
 resource "azurerm_virtual_machine" "vm" {
