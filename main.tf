a2f82f2b063da940a9700eeaafe2fd8bf2189bb2
James-2025 <4al20cs052@gmail.com>
2024-11-03 16:45:14 +0530

    c04

--- main.tf
+++ main.tf
@@ -1,7 +1,19 @@
 #rgruigufy
 provider "azurerm" {
   features {}
+
+  # Reference the required provider properties from environment variables
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
+
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+
 
 variable "vm_name" {
   default = "my-azure-vm"
