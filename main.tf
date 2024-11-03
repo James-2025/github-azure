a545ada801413fc0a07c176c74baa9288095a5f8
James-2025 <4al20cs052@gmail.com>
2024-11-03 18:48:50 +0530

    c08

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
-  size                  = "Standard_B1s"
+  size                  = "Standard_B16pls_v2"
 
   os_disk {
     caching              = "ReadWrite"
